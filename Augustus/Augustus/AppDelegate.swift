464e24d77d2231a396d6263d788ee5bf7a1ed873
ryanglobus <ryanglobus@gmail.com>
2015-06-22 18:09:50 -0700

    Created empty Mac OS X Xcode project

---
+++ Augustus/Augustus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Augustus
+//
+//  Created by Ryan Globus on 6/22/15.
+//  Copyright (c) 2015 Ryan Globus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
