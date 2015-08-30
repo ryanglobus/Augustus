ae84e70335cecc67b5b05feb57a3498850a6c473
ryanglobus <ryanglobus@gmail.com>
2015-08-30 12:12:53 -0700

    AUEventField Clipping Bug: Partial Fix
    Now if events are just right width, they will not be clipped. However,
    now if events are slightly narrower (e.g. “Brunch with Rupii”) events
    will be too tall.

--- Augustus/Augustus/AUEventField.swift
+++ Augustus/Augustus/AUEventField.swift
@@ -47,9 +47,9 @@ class AUEventField: NSTextField {
         layoutManager.addTextContainer(container)
         storage.addLayoutManager(layoutManager)
         storage.addAttribute(NSFontAttributeName, value: font, range: NSMakeRange(0, storage.length))
-        container.lineFragmentPadding = 0.0
+        container.lineFragmentPadding = 3.0 // 3.0 to prevent text at same width from being cut off
         layoutManager.glyphRangeForTextContainer(container)
-        let height = layoutManager.usedRectForTextContainer(container).size.height + font.pointSize / 3 // add font.pointSize / 2 for letters like 'y' and 'g'
+        let height = layoutManager.usedRectForTextContainer(container).size.height + font.pointSize / 3 // add font.pointSize / 3 for letters like 'y' and 'g'
         
         let frame = NSRect(origin: origin, size: CGSize(width: width, height: height))
         super.init(frame: frame)
